c42059bf82b5cf7c0e3c03c8043b5e5ab1339e8b
soarflat <soarflat777@gmail.com>
2019-11-23 16:27:10 +0900

    feat: Realtime Database を利用したメモの編集機能を実装

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,43 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { uuid } from 'uuidv4';
 
-const PageComponent: React.FC<any> = () => <div>pages/Index</div>;
+import { Link } from '../Router';
+import { useAllDocuments } from '../FirebaseDatabase';
 
-export default PageComponent;
+const useDocumentLists = () => {
+  const { document: documents, loaded } = useAllDocuments();
+
+  return useMemo(() => {
+    if (!loaded) {
+      return <div>documents loading now</div>;
+    }
+    if (!documents) {
+      return [];
+    }
+    return Object.keys(documents).map(textId => {
+      const title = documents[textId].title;
+      return (
+        <li key={textId}>
+          <Link as="a" href={`/${textId}`}>
+            {title}
+          </Link>
+        </li>
+      );
+    });
+  }, [documents, loaded]);
+};
+
+const IndexPage = () => {
+  const list = useDocumentLists();
+
+  return (
+    <div>
+      <Link as="button" href={`/${uuid()}`}>
+        ページ作成
+      </Link>
+      <ul>{list}</ul>
+    </div>
+  );
+};
+
+export default IndexPage;
